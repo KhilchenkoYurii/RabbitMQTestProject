1d16175d90fc867308b4d4cc061e0dbfb7d847d0
Yurii Khilchenko <yurii.khilchenko@everymatrix.com>
2026-03-28 14:13:37 +0200

    Try Exchange to Exchange binding

---
+++ ExToExDemo/Program.cs
@@ -0,0 +1,67 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExToExDemo
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.VirtualHost = "/";
+            factory.Port = 5672;
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            var connection = await factory.CreateConnectionAsync();
+
+            var channel = await connection.CreateChannelAsync();
+
+            await channel.ExchangeDeclareAsync(
+                "exchange1",
+                "direct",
+                true,
+                false,
+                null);
+
+            await channel.ExchangeDeclareAsync(
+                "exchange2",
+                "direct",
+                true,
+                false,
+                null);
+
+            await channel.QueueDeclareAsync(
+                "queue1",
+                true,
+                false,
+                false,
+                null);
+            await channel.QueueDeclareAsync(
+                "queue2",
+                true,
+                false,
+                false,
+                null);
+
+            await channel.QueueBindAsync("queue1", "exchange1", "key1");
+            await channel.QueueBindAsync("queue2", "exchange2", "key2");
+            await channel.ExchangeBindAsync("exchange2", "exchange1", "key2");
+
+            await channel.BasicPublishAsync(
+                "exchange1",
+                "key1",
+                Encoding.UTF8.GetBytes("Message with key 1"));
+
+            await channel.BasicPublishAsync(
+                "exchange1",
+                "key2",
+                Encoding.UTF8.GetBytes("Message with key2"));
+        }
+    }
+}
